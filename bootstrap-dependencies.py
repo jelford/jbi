d030f3495f3774e6590a9443e04e35a8c0d73256
James Elford <james.p.elford@gmail.com>
2014-08-22 15:47:13 +0100

    Bootstrap script checks for local shared libs

--- bootstrap-dependencies.py
+++ bootstrap-dependencies.py
@@ -1,13 +1,44 @@
 #! /usr/bin/env python3
 
-from subprocess import call
+from subprocess import Popen, PIPE
+from os import path
+import shutil
+
+lib_directory = 'lib'
+
+def copy(source, target_directory):
+    shutil.copy(source, target_directory)
+
+def lib_present(name):
+    for extension in ('.a', '.so'):
+        if path.isfile(path.join(lib_directory, 'lib{name}{extension}'.format(**locals()))):
+            return True
+    return False
 
 def locate(file_name):
-    return call(['locate', file_name]) == 0
+    p = Popen(['locate', file_name], stdout=PIPE, stdin=PIPE)
+    out, err = p.communicate()
+    if p.returncode == 0 and out:
+        return out.decode()
+    else:
+        return False
+        
 
-def require_shared_library(name):
-    if not locate('lib{name}.so'.format(name=name)):
-        raise RuntimeError('Can\'t find required library "{name}" on your system - aborting')
+def require_installed_shared_library(name):
+    library_name = 'lib{name}.so'.format(name=name)
+    if not locate(library_name):
+        raise RuntimeError(
+            'Can\'t find required library "{name}" on your system - aborting'.format(name=name))
+
+def require_local_shared_library(name):
+    if lib_present(name):
+        return
+
+    raise RuntimeError(
+            'Can\'t find required library "{name}" in "{lib_directory}". You\'ll need to get a copy.'
+                .format(name=name, lib_directory=lib_directory))
 
 if __name__ == '__main__':
-    require_shared_library('boost_unit_test_framework')
+    require_installed_shared_library('boost_unit_test_framework')
+    require_local_shared_library('gmock')
+    require_local_shared_library('gtest')
